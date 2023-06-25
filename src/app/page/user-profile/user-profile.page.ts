f45c85d85dea33921ddbd073ef1eb0f82b7f5bbd
Anthony-svg <anthony.melendez07.am@gmail.com>
2023-06-24 22:46:51 -0500

    carga de campos del usuario

--- src/app/page/user-profile/user-profile.page.ts
+++ src/app/page/user-profile/user-profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataSharingService } from '../../services/data-sharing.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,16 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.page.scss'],
 })
 export class UserProfilePage implements OnInit {
-
+  
+  private receivedData: any;
   names!: string;
   username!: string;
   phone!: string;
   email!: string;
+
   
-  constructor() { }
+  constructor(private dataSharingService: DataSharingService) { }
 
   ngOnInit() {
+    this.receivedData = this.dataSharingService.getJsonData();
+    this.names = this.receivedData.nombrePers;
+    this.username = this.receivedData.usernamePers;
+    this.phone = this.receivedData.telfPers;
+    this.email = this.receivedData.correoPers;
   }
+
 
   editNames() {
 
