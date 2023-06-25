8228d6aa20a099deffc4fd6d2d9eaef7ee836b63
Jean Pierre <jean_0720@hotmail.com>
2023-06-24 21:45:34 -0500

    Avance Perfil de usuario

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { DataSharingService } from '../../services/data-sharing.service';
+import { MenuController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent  implements OnInit {
+export class MenuComponent implements OnInit {
   public items = [
     { name: 'Inicio', image: 'assets/casa.svg' },
     { name: 'Boletos Comprados', image: 'assets/boleto.svg' },
     { name: 'Comprar Boletos', image: 'assets/compra.svg' },
     { name: 'Destinos', image: 'assets/destino.svg' },
     { name: 'Perfil', image: 'assets/user.svg' },
-    { name: 'Cerrar Sesión', image: 'assets/cerrar.svg' }
+    { name: 'Cerrar Sesión', image: 'assets/cerrar.svg' },
   ];
-  constructor() { }
 
-  ngOnInit() {}
+  private receivedData: any;
+  namesUser!: string;
+  rolUser!: string;
+
+  constructor(private dataSharingService: DataSharingService,
+                private menuController: MenuController, private navCtrl: NavController) {}
+
+  ngOnInit() { }
+
+  onMenuOpen() {
+    this.receivedData = this.dataSharingService.getJsonData();
+    this.namesUser = this.receivedData.nombrePers;
+    this.rolUser = this.receivedData.rolPers;
+  }
+
+  onMenuItemClick(item: any) {
+    this.navCtrl.navigateForward('/user-profile');
+    this.menuController.close();
+  }
 
 }
---
+++ src/app/services/data-sharing.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataSharingService {
+
+  private userData: any;
+
+  constructor() { }
+
+  setJsonData(data: any) {
+    this.userData = data;
+  }
+
+  getJsonData() {
+    return this.userData;
+  }
+
+}
