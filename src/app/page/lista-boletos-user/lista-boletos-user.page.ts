5a3877a3325b2e205db481f0d50c919445327077
Anthony-svg <anthony.melendez07.am@gmail.com>
2023-07-09 21:49:06 -0500

    #21 Diseño pagina visualización de boletos

--- src/app/page/lista-boletos-user/lista-boletos-user.page.ts
+++ src/app/page/lista-boletos-user/lista-boletos-user.page.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-boletos-user.page.scss'],
 })
 export class ListaBoletosUserPage implements OnInit {
+  public itemsRoutesFilter=[
+    { ciudadFinRuta: 'Nombre destino', numBus:"1", cooperativa:"Santa",fechaHora:"fecha y hora"},
 
+  ];
   constructor() { }
 
   ngOnInit() {
