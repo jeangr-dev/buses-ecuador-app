4ebfa39b9eef8f82b24a1394ee2b5908ada7df36
jean96gr <jean96gr@gmail.com>
2023-07-12 14:51:49 -0500

    #30 Arreglo de errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { PayPal } from '@ionic-native/paypal/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -14,7 +15,7 @@ import { MenuComponent } from './components/menu/menu.component';
 @NgModule({
   declarations: [AppComponent,MenuComponent],
   imports: [BrowserModule, IonicModule.forRoot(), HttpClientModule, AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, PayPal],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, PayPal, File],
   bootstrap: [AppComponent],
   exports:[MenuComponent],
 })
